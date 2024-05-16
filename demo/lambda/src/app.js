764f50f00bd450351ccc8ff269d810fd33422267
timam <mail@timam.io>
2024-05-16 17:02:46 +0600

    Update response structure in Lambda and AWS region in deploy workflow
    The response structure in Lambda's app.js has been updated by adding a comma to correctly format the returned JSON data. Also, changed the method of accessing AWS region in the deployment workflow to utilize GitHub secrets instead of environment variables for better security.

--- demo/lambda/src/app.js
+++ demo/lambda/src/app.js
@@ -1,6 +1,6 @@
 exports.handler = async (event) => {
     const response = {
-        statusCode: 200
+        statusCode: 200,
         body: JSON.stringify('Hello from Lambda!')
     };
     return response;
